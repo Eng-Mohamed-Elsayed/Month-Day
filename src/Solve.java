6777b11186ef2442ff4a7caae10f87eb37830def
course <mohamed.elsayed.course@gmail.com>
2024-03-21 13:28:36 +0300

    ADD Task mohamed and mai

---
+++ src/Solve.java
@@ -0,0 +1,19 @@
+public class Solve {
+    public static void main(String[] args) {
+        /*
+           TODO: (Mai)
+             - create method  called  (getNameMonth)  return month
+             - read number month from user (Scanner)
+             - condition using  switch
+             - return method String
+         */
+
+        /*
+           TODO: (Mohamed)
+               - create method (getNameDay) return day
+               - read number day form user (Scanner)
+               - condition using  switch
+               - return method String
+         */
+    }
+}
